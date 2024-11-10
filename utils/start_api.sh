b95c72def5b9f4bfc634a388267e9bfbe4cd3534
Mekan777-alt <mekan2678@gmail.com>
2024-11-10 14:57:24 +0300

    fixing start api to uvicorn server

--- utils/start_api.sh
+++ utils/start_api.sh
@@ -4,12 +4,4 @@ alembic upgrade head
 
 export PYTHONUNBUFFERED=1
 
-# Запуск Gunicorn с оптимизированными параметрами
-gunicorn src.api_app:app \
-    -w 3 \
-    -k uvicorn.workers.UvicornWorker \
-    --threads 2 \
-    --timeout 30 \
-    --max-requests 1000 \
-    --max-requests-jitter 100 \
-    --bind 0.0.0.0:8000
+uvicorn src.api_app:app --host 0.0.0.0 --port 8000
