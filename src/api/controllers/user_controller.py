1e0250f301670a2380f454278df98ac85db6b602
Mekan777-alt <mekan2678@gmail.com>
2024-12-28 23:35:38 +0300

    fixing user patch endpoint

--- src/api/controllers/user_controller.py
+++ src/api/controllers/user_controller.py
@@ -26,21 +26,21 @@ async def get_me_info_endpoint(current_user: Annotated[dict, Depends(get_user_fr
               summary="Частичное обновление информации о пользователе")
 async def partial_update_user_endpoint(
     current_user: Annotated[dict, Depends(get_user_from_token)],
-    first_name: Optional[str] = Form(None),
-    last_name: Optional[str] = Form(None),
-    phone_number: Optional[str] = Form(None),
+    firstName: Optional[str] = Form(None),
+    lastName: Optional[str] = Form(None),
+    phoneNumber: Optional[str] = Form(None),
     location: Optional[str] = Form(None),
-    about_my_self: Optional[str] = Form(None),
+    aboutMySelf: Optional[str] = Form(None),
     photo: Optional[UploadFile] = File(None),
     service: UserService = Depends(get_user_service),
     minio_client: MinioClient = Depends(get_minio_client)
 ):
     return await service.partial_update_service(current_user=current_user,
-                                                first_name=first_name,
-                                                last_name=last_name,
-                                                phone=phone_number,
+                                                first_name=firstName,
+                                                last_name=lastName,
+                                                phone=phoneNumber,
                                                 location=location,
-                                                about_my_self=about_my_self,
+                                                about_my_self=aboutMySelf,
                                                 photo=photo,
                                                 minio_client=minio_client
                                                 )
