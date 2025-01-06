c515c6cef0bda9191480e4dac9620124eeb93f37
Mekan777-alt <mekan2678@gmail.com>
2025-01-06 06:32:26 +0300

    remove firebase sdk

--- src/api/firebase/utils.py
+++ src/api/firebase/utils.py
@@ -2,7 +2,7 @@ import firebase_admin
 import os
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(f"{os.getcwd()}/generaworker-firebase-adminsdk-49tht-8d334ad9df.json")
+cred = credentials.Certificate(f"{os.getcwd()}/app-freelance-f3dee-firebase-adminsdk-8d92q-53823e41e1.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
