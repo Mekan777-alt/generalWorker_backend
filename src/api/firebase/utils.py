a0ccb334da4286aca946cf0ba2c9568a7e85f179
Mekan777-alt <mekan2678@gmail.com>
2025-02-03 22:58:05 +0300

    update sdk firebase

--- src/api/firebase/utils.py
+++ src/api/firebase/utils.py
@@ -2,7 +2,7 @@ import firebase_admin
 import os
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(f"{os.getcwd()}/app-freelance-f3dee-firebase-adminsdk-8d92q-f8deea39b3.json")
+cred = credentials.Certificate(f"{os.getcwd()}/freelance-f82f0-firebase-adminsdk-fbsvc-fa13706488.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
